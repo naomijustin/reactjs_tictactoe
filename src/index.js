4d33cd147df6c9c6794f490745955588b04a40f8
Naomi Justin <naomi.z.justin@gmail.com>
2022-12-30 12:05:16 +1100

    Add the handleClick function

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+class Square extends React.Component {  
     render() {
       return (
         <button 
         className="square" 
-        onClick={() => this.setState({value: 'X'})}
-        >  {this.state.value}
+        onClick={() => this.props.onClick()}
+        >  {this.props.value}
         </button>
       );
     }
@@ -26,6 +20,11 @@ class Square extends React.Component {
       this.state = {
         squares: Array(9).fill(null),
       };
+    }
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
     }
     renderSquare(i) {
       return (<Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>);
