d89417010ce7da3b026fc1c80e723734c215e32a
Naomi Justin <naomi.z.justin@gmail.com>
2023-01-09 07:21:31 +1100

    Removed comment of previous Square class

--- src/index.js
+++ src/index.js
@@ -3,25 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-function Square(props){
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-// class Square extends React.Component {  
-//     render() {
-//       return (
-//         <button 
-//         className="square" 
-//         onClick={() => this.props.onClick()}
-//         >  {this.props.value}
-//         </button>
-//       );
-//     }
-//   }
+  function Square(props){
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
   
   class Board extends React.Component {
     constructor(props) {
