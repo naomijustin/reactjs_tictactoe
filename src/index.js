65105ccfae2b55552a1a473d0b506a2e94094877
Naomi Justin <naomi.z.justin@gmail.com>
2023-01-10 07:16:25 +1100

    Switch between X and O via the handleClick function

--- src/index.js
+++ src/index.js
@@ -21,8 +21,11 @@ import './index.css';
     }
     handleClick(i){
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';      
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
     renderSquare(i) {
       return (<Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>);
