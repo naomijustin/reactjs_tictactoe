989801bb9a06560f9d0bb6d572f5e3d142a5bcd7
naomijustin <naomi.z.justin@gmail.com>
2023-02-21 17:54:14 +1100

    Adding step number to the Game constructor

--- src/index.js
+++ src/index.js
@@ -43,7 +43,8 @@ import './index.css';
       this.state = {
         history: [{
           squares: Array(9).fill(null),
-        }],          
+        }],
+          stepNumber: 0,          
           xIsNext: true        
       }
     }
