4416a592588085082eb82d11f7673c4b5dc15a8a
naomijustin <naomi.z.justin@gmail.com>
2023-02-23 07:26:52 +1100

    Button click goes back to selected state in history

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ import './index.css';
       }
     }
     handleClick(i){
-      const history = this.state.history;      
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);      
       const current = history[history.length - 1]; 
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
@@ -58,8 +58,9 @@ import './index.css';
       squares[i] = this.state.xIsNext ? 'X' : 'O';      
       this.setState({
         history: history.concat([{
-          squares: squares,
-        }]),        
+          squares: squares,          
+        }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
     }
